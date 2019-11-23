d0a232742baa70b2daccc6f57525e4c955cbb178
Louis <sierkinhane@163.com>
2019-11-23 17:52:58 +0800

    Add files via upload

---
+++ 01-UnsupervisedLearning/CGAN/mnist.py
@@ -0,0 +1,22 @@
+"""
+keras: MNIST handwritten digits dataset.
+"""
+import numpy as np
+
+
+def load_data(path='mnist.npz'):
+    """Loads the MNIST dataset.
+
+    # Arguments
+        path: path where to cache the dataset locally
+            (relative to ~/.keras/datasets).
+
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    path = "../data/mnist.npz"
+    f = np.load(path)
+    x_train, y_train = f['x_train'], f['y_train']
+    x_test, y_test = f['x_test'], f['y_test']
+    f.close()
+    return (x_train, y_train), (x_test, y_test)
